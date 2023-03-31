2cc4b950637c65d1a007ae272fdb824fd4c37b8a
Kaveri Durga Appana <kaveri.appana@qualitestgroup.com>
2023-03-31 11:41:45 +0530

    commitmaster

---
+++ src/test/java/EmployeeDummyData/GetAllEmployes.java
@@ -0,0 +1,29 @@
+package EmployeeDummyData;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class GetAllEmployes {
+    @Test
+    public void getallemployess(){
+        RestAssured.baseURI="https://dummy.restapiexample.com";
+        RequestSpecification httprequest = RestAssured.given();
+        Response response = httprequest.request(Method.GET,"/api/v1/employees");
+
+        String header=response.getHeader("Content-Type");
+        System.out.println("header is "+header);
+      
+
+        Assert.assertEquals(response.getStatusCode(),200);
+
+        String body = response.getBody().asString();
+        System.out.println("response body is "+body);
+
+        Assert.assertTrue(body.contains("\"status\":\"success\""));
+
+    }
+}
